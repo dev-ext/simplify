7b14b908a723b25279ce47cb419719f30833b382
webgrity69 <webgrity69@gmail.com>
2014-02-15 10:07:38 +0530

    ## quantacast and google analytic add

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 // JavaScript Document
+//<!-- Quantcast Tag -->
+var _qevents = _qevents || [];
+(function() {
+var elem = document.createElement('script');
+elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+elem.async = true;
+elem.type = "text/javascript";
+var scpt = document.getElementsByTagName('script')[0];
+scpt.parentNode.insertBefore(elem, scpt);
+})();
+_qevents.push({
+qacct:"p-k-uYCqAKWQT--"
+});
+//<!-- End Quantcast tag -->
+/* Google Analytic */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','devtool');
+
+devtool('create', 'UA-XXXX-Y', 'auto');
+devtool('send', 'pageview');
+/* end Google Analytic */
+
+
 //## All site fuctionality javascript 
 
 
