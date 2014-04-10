312369da3972b4c2c87c5825a75212c34ca2492a
webgrity69 <webgrity69@gmail.com>
2014-04-10 17:37:47 +0530

    shortcode example added .

--- base-template/js/main.js
+++ base-template/js/main.js
@@ -67,7 +67,12 @@ jQuery('.drw').removeClass('drw');
 
 
 });
-
+jQuery(".entry").shortcode({
+  content: function(el, options, content) {
+    return "<p>" + content + "</p>";
+  }
+});
+// usage [content]Cool content[/content]
 }
 site.analytic = function (uaid) {
 eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(3(i,s,o,g,r,a,m){i[\'5\']=r;i[r]=i[r]||3(){(i[r].q=i[r].q||[]).6(7)},i[r].l=1*8 9();a=s.b(o),m=s.c(o)[0];a.d=1;a.e=g;m.f.h(a,m)})(j,k,\'n\',\'//p.t-4.u/4.v\',\'2\');2(\'w\',x,\'y\');2(\'z\',\'A\');',37,37,'||devtool|function|analytics|GoogleAnalyticsObject|push|arguments|new|Date||createElement|getElementsByTagName|async|src|parentNode||insertBefore||window|document|||script||www||||google|com|js|create|uaid|auto|send|pageview'.split('|'),0,{}))
