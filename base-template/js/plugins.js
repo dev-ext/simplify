5107735b9c3f75a5d9b536fb2bb8ddae8379aa0d
webgrity69 <webgrity69@gmail.com>
2014-04-17 11:02:51 +0530

    ## update.

--- base-template/js/plugins.js
+++ base-template/js/plugins.js
@@ -13,3 +13,20 @@ jQuery(window).bind('resizeEnd', function() {
 });
 
 */
+
+//Found in www.stackoverflow.com//
+jQuery.fn.ToggleInputValue = function(){
+    return jQuery(this).each(function(){
+        var Input = jQuery(this);
+        var default_value = Input.val();
+
+        Input.focus(function() {
+           if(Input.val() == default_value) Input.val("");
+        }).blur(function(){
+            if(Input.val().length == 0) Input.val(default_value);
+        });
+    });
+}
+/*jQuery(document).ready(function(){
+    jQuery('.toggle').ToggleInputValue();
+});*/
