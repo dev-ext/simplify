8aa53f32b0bfe6af11e88a44570ef2ecde7e8c0f
webgrity69 <webgrity69@gmail.com>
2014-04-25 15:51:46 +0530

    debug.js mod

--- base-template/js/debug.js
+++ base-template/js/debug.js
@@ -5,10 +5,13 @@ function checksite() {
 var s = {};
 // check has h1	
 s.h1 = jQuery("h1")	;
-if (s.h1.length=='' || s.h1.length==null || s.h1.length>0) {
-	console.log("H1 Tag problem");
+if (s.h1.length=='' || s.h1.length==null || s.h1.length>1) {
+	console.log(s.h1.length,"H1 Tag problem");
 	}
 // check entry 
-
+s.entry = jQuery(".entry");
+if (s.entry.length=='' || s.entry.length==null || s.entry.length>1) {
+	console.log(s.entry.length,"Check HTML structure.");
+	}
 	
 	}
